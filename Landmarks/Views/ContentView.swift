52794c649210deb8a5e6a1f169c30de9aca998fd
wilsolutions <wils.solutions@gmail.com>
2021-10-01 09:13:52 -0400

    Added Categories and Navigation between sections

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  
+  @State private var selection: Tab = .featured
+  
+  enum Tab {
+    case featured
+    case list
+  }
+  
+  var body: some View {
+    TabView(selection: $selection) {
+      CategoryHome()
+        .tabItem {
+          Label("Featured", systemImage: "star")
+        }
+        .tag(Tab.featured)
+
       LandmarkList()
+        .tabItem {
+          Label("List", systemImage: "list.bullet")
+        }
+        .tag(Tab.list)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
