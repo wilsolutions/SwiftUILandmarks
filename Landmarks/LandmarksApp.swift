d876fa90e16035b8f7590865b419c9c72e12958c
Wils <wils.solutions@gmail.com>
2021-10-16 16:46:57 -0400

    Add macOS target and respective changes

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -11,10 +11,14 @@ import SwiftUI
 struct LandmarksApp: App {
   @StateObject private var modelData = ModelData()
   
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-            .environmentObject(modelData)
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+      .environmentObject(modelData)
     }
+    
+    #if os(watchOS)
+    WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+    #endif
+  }
 }
--- WatchLandmarks WatchKit Extension/LandmarkDetail.swift
+++ WatchLandmarks WatchKit Extension/LandmarkDetail.swift
@@ -16,15 +16,49 @@ struct LandmarkDetail: View {
   }
   
     var body: some View {
-      CircleImage(image: landmark.image.resizable())
-          .scaledToFill()
+      ScrollView {
+        VStack {
+            CircleImage(image: landmark.image.resizable())
+                .scaledToFit()
+            
+            Text(landmark.name)
+                .font(.headline)
+                .lineLimit(0)
+
+            Toggle(isOn: $modelData.landmarks[landmarkIndex].isFavorite) {
+                Text("Favorite")
+            }
+
+            Divider()
+          
+          MapView(coordinate: landmark.locationCoordinate)
+              .scaledToFit()
+
+            Text(landmark.park)
+                .font(.caption)
+                .bold()
+                .lineLimit(0)
+
+            Text(landmark.state)
+                .font(.caption)
+        }
+        .padding(16)
+      }
+      .navigationTitle("Landmarks")
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
       let modelData = ModelData()
-      return LandmarkDetail(landmark: modelData.landmarks[0])
+      return Group {
+        LandmarkDetail(landmark: modelData.landmarks[0])
           .environmentObject(modelData)
+          .previewDevice("Apple Watch Series 7 - 45mm")
+        
+        LandmarkDetail(landmark: modelData.landmarks[1])
+          .environmentObject(modelData)
+          .previewDevice("Apple Watch Series 7 - 41mm")
+      }
     }
 }
