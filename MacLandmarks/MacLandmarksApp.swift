3cce18bc0fd8b8a17fba23713e580d303c4f64bf
Wils <wils.solutions@gmail.com>
2021-10-16 16:46:22 -0400

    Apple watch updates

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Wilson Gomes on 2021-10-16.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
